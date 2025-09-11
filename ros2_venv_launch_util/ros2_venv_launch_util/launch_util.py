e57bc2135bf2bcb42f0b5a82fb6020ce6ebbf278
md26woxu <marekdaniv@googlemail.com>
2025-09-11 11:12:04 +0200

    Update launch util for new venv naming scheme

--- ros2_venv_launch_util/ros2_venv_launch_util/launch_util.py
+++ ros2_venv_launch_util/ros2_venv_launch_util/launch_util.py
@@ -1,16 +1,21 @@
 import subprocess
 from ament_index_python.resources import get_resource
+from ament_index_python.packages import get_package_share_directory
 import sys
 import os
 
 
-def check_venv_compatibility(venv_dir: str) -> bool:
+def check_venv_compatibility(pkg_name: str) -> bool:
+    venv_dir = os.path.join(get_package_share_directory(pkg_name), "..", "..", "venv")
+
     compatibility_script_path = os.path.join(
         get_resource("ros2_venv_utility_scripts", "ros2_venv_scripts")[0],
         "check_venv_compatibility.bash",
     )
     compatibility_result = subprocess.run(
-        ["bash", compatibility_script_path, venv_dir], capture_output=True, text=True
+        ["bash", compatibility_script_path, venv_dir, pkg_name],
+        capture_output=True,
+        text=True,
     )
 
     if compatibility_result.returncode == 1:
@@ -21,16 +26,16 @@ def check_venv_compatibility(venv_dir: str) -> bool:
         sys.exit(1)
 
 
-def add_venv_to_current_env(pkg_share: str) -> dict:
+def add_venv_to_current_env(pkg_name: str) -> dict:
     """
     Build environment dict for launching node in a virtualenv,
     preserving existing environment variables.
     """
     env = os.environ.copy()
-    venv_dir = os.path.join(pkg_share, "..", "..", "venv")
-    venv_path = os.path.join(venv_dir, "py_venv")
+    venv_dir = os.path.join(get_package_share_directory(pkg_name), "..", "..", "venv")
+    venv_path = os.path.join(venv_dir, pkg_name)
 
-    check_venv_compatibility(venv_dir)
+    check_venv_compatibility(pkg_name)
 
     python_version = f"python{sys.version_info.major}.{sys.version_info.minor}"
     site_packages = os.path.join(venv_path, "lib", python_version, "site-packages")
