3ebddeedb58c9c1585b5b2d571e6958c3538772b
md26woxu <marekdaniv@googlemail.com>
2025-09-18 21:19:43 +0200

    Improve logging for venv setup. Fix isolated arg evaluation

--- ros2_venv_scripts/bash/setup_env.bash
+++ ros2_venv_scripts/bash/setup_env.bash
@@ -6,21 +6,23 @@ cd $1/venv
 
 # Clean up old virtual environment if it exists
 rm -rf "$PROJECT_NAME"
+:> $LOGFILE
 
-if [[ $2 -eq "--isolated" ]]; then
+if [[ $2 == "--isolated" ]]; then
     # Create a fully independent virtual environment
-    tee -a "$LOGFILE" "Creating an isolated virtual environment in $PROJECT_NAME"
+    echo "Creating an isolated virtual environment in $PROJECT_NAME" >> $LOGFILE
     python3 -m venv "$PROJECT_NAME"
 else
     # Create a virtual environment with access to system site packages
+    echo "Creating an virtual environment with access to system packages in $PROJECT_NAME" >> $LOGFILE
     python3 -m venv "$PROJECT_NAME" --system-site-packages
 fi
 
 if [[ ! -f "requirements.txt" ]]; then
-    tee -a "$LOGFILE" "Error: $1/venv/requirements.txt not found!"
+    echo "Error: $1/venv/requirements.txt not found!" >> $LOGFILE
     exit 1
 fi
 
-source "$PROJECT_NAME/bin/activate"
-pip3 install -r requirements.txt > "$LOGFILE"
+source "$PROJECT_NAME/bin/activate" || exit 1
+pip3 install -r requirements.txt >> "$LOGFILE"
 deactivate
